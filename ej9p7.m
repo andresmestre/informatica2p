3e2eecb525a5987541bf375d4c82e7cb0cf0f9b5
andresmestre <andresmestr3@gmail.com>
2025-01-03 12:56:39 +0100

    Add files via upload

---
+++ ej9p7.m
@@ -0,0 +1,9 @@
+% Ejercicio 9.7.- Saltando datos con %"s 
+% El fichero de datos lectura_maquina.txt, que se puede descargar del repositorio, contiene información sobre parámetros que transmite 
+% cierta máquina de un proceso industrial. El fichero, tras una línea de encabezamiento, tiene en cada línea la siguiente información: 
+% tres números reales y una cadena de texto a continuación, con número de caracteres indeterminado. Escriba un programa que lea el 
+% tercer dato numérico de cada línea del fichero, guardando todos en un vector. El programa debe escribir en pantalla el valor máximo 
+% y mínimo de la tercera columna del fichero. 
+git add lectura_maquina.txt
+git commit -m "Añadir lectura_maquina.txt"
+git push origin main
